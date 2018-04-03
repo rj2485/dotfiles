a386bb2dceba07001c4e9d7ff4c68f0f16393a19
Rodda John <rj2485@columbia.edu>
2018-04-03 12:53:13 -0400

    First commit

---
+++ init.el
@@ -0,0 +1,35 @@
+;; Setup melpa
+
+(require 'package)
+
+(setq package-enable-at-startup nil)
+(setq package-archives
+	     '(("melpa" . "https://melpa.org/packages/")
+	       ("gnu" . "https://elpa.gnu.org/packages/")
+	       ("org" . "http://orgmode.org/elpa/")
+	     ))
+
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+;; Setup use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (htmlize ox-reveal zenburn-theme which-key use-package try sml-mode org-bullets nlinum load-theme-buffer-local jinja2-mode counsel auto-complete ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
